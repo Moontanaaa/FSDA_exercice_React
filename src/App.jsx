be4be824d74b144747b97e316984119b2246048e
Moontanaaa <montanafabio@icloud.com>
2025-01-06 12:26:13 +0100

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Avatar from './avatar'
 function App() {
   const users = [
     {
-      image: "https://static.wikia.nocookie.net/simpsons/images/a/a1/Marge_Simpsons.png/revision/latest?cb=20130817213926&path-prefix=fr",
+      key : 1,
+      image: 'https://static.wikia.nocookie.net/simpsons/images/a/a1/Marge_Simpsons.png/revision/latest?cb=20130817213926&path-prefix=fr',
       firstName: "Marge",
       lastName: "Simpson"
     },
-    {
+    { 
+      key : 2,
       image: "https://static.wikia.nocookie.net/simpsons/images/3/33/Homer.png/revision/latest?cb=20110703093455&path-prefix=fr",
       firstName: "Homer",
       lastName: "Simpson"
